eb23272afa94273c0e0bd1e5e6525106ea868c16
ifox6677 <zhangjq0908@outlook.com>
2026-02-11 18:15:31 +0800

    Update ForecastCityActivity.java

--- app/src/main/java/org/woheller69/weather/activities/ForecastCityActivity.java
+++ app/src/main/java/org/woheller69/weather/activities/ForecastCityActivity.java
@@ -118,12 +118,10 @@ public class ForecastCityActivity extends NavigationActivity implements IUpdatea
                 SharedPreferences prefManager = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
                 SQLiteHelper database = SQLiteHelper.getInstance(getApplicationContext().getApplicationContext());
                 CurrentWeatherData currentWeather = database.getCurrentWeatherByCityId(pagerAdapter.getCityIDForPos(position));
-
-                long timestamp = currentWeather.getTimestamp();
-                long systemTime = System.currentTimeMillis() / 1000;
-                //long updateInterval = (long) (Float.parseFloat(prefManager.getString("pref_updateInterval", "2")) * 60 * 60);
-                long updateInterval = (long) (0.5 * 60 * 60);
-                if (timestamp + updateInterval - systemTime <= 0) {
+                long timestamp = currentWeather.getTimestamp();   // 如果它是毫秒就直接用
+                long systemTime = System.currentTimeMillis();     // 毫秒
+                long updateInterval = (long) (0.25 * 60 * 60 * 1000); // 毫秒
+                 if (timestamp + updateInterval - systemTime <= 0) {
                     if (pagerAdapter.getCityIDForPos(position)!=getWidgetCityID(context)||locationListenerGPS==null) { //do not update first TAB while location is updating
                         WeatherPagerAdapter.refreshSingleData(getApplicationContext(),true, pagerAdapter.getCityIDForPos(position));
                         ForecastCityActivity.startRefreshAnimation();
